73c3aa07d0fa4ffa15ff7eed7f1dfb1cdf681ec0
Chlorate <me@chlorate.ca>
2017-08-22 23:49:26 -0300

    Add basic feature checks

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import {render} from "inferno";
 
 const IndexComponent = () => {
+	if (!navigator.getGamepads) {
+		return (
+			<div class="alert alert-danger text-center m-3" role="alert">
+				Your browser doesn't support the Gamepad API.
+			</div>
+		);
+	}
+
 	return <div>Input Display</div>;
 };
 

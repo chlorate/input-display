6775a4f0a1c74618861d33166a710ec948822f39
Chlorate <me@chlorate.ca>
2017-09-30 22:25:34 -0300

    Test clampInt

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 import {observable} from "mobx";
-import {clampIntOrNull} from "./math";
+import {clampInt} from "./math";
 
 export class Config {
 	@observable private _hatAxis: number | null = null;
 
 	get hatAxis(): number | null { return this._hatAxis; }
-	set hatAxis(value: number | null) { this._hatAxis = clampIntOrNull(value, 0); }
+	set hatAxis(value: number | null) { this._hatAxis = value === null ? null : clampInt(value, 0); }
 }
--- src/math.ts
+++ src/math.ts
@@ -1,14 +1,17 @@
-export function clampIntOrNull(n: any, min?: number, max?: number): number | null {
-	if (n === null) {
-		return n;
+export function clampInt(n: any, min?: number, max?: number): number {
+	if (min !== undefined && max !== undefined && min > max) {
+		throw new RangeError("min must be less than max");
 	}
 
 	n = parseInt(n, 10);
+	if (isNaN(n)) {
+		n = 0;
+	}
 	if (min !== undefined && n < min) {
 		n = min;
 	}
 	if (max !== undefined && n > max) {
 		n = max;
 	}
-	return isNaN(n) ? null : Math.trunc(n);
+	return Math.trunc(n);
 }

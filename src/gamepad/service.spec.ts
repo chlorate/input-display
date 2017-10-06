829a7fc8a287c5624a7e2cf8f36919e6c9041fcc
Chlorate <me@chlorate.ca>
2017-10-05 21:08:36 -0300

    Fix test

--- src/gamepad/service.spec.ts
+++ src/gamepad/service.spec.ts
@@ -2,6 +2,11 @@ import {getGamepadIds} from "./service";
 
 describe("gamepad/service", () => {
 	beforeEach(() => {
+		// PhantomJS shim:
+		if (!navigator.getGamepads) {
+			navigator.getGamepads = () => {};
+		}
+
 		spyOn(navigator, "getGamepads").and.returnValue([
 			{id: "Unknown Gamepad (Vendor: 057e Product: 0306)"},
 			null,

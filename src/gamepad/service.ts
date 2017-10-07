67e23b3e8ce13809e236c3e3db54a0a016d29142
Chlorate <me@chlorate.ca>
2017-10-07 02:08:07 -0300

    Cleanup

--- src/gamepad/service.ts
+++ src/gamepad/service.ts
@@ -2,18 +2,32 @@ const aliases: {[id: string]: string} = {
 	"Unknown Gamepad (Vendor: 057e Product: 0306)": "Wii Remote",
 };
 
-export function getGamepads(): Array<Gamepad | null> {
-	return navigator.getGamepads();
-}
-
-export function getGamepadIds(): Array<string | undefined> {
-	const ids: string[] = [];
+/**
+ * Returns an array of gamepads from the Gamepad API.
+ */
+export function getGamepads(): Array<Gamepad | undefined> {
+	// navigator.getGamepads doesn't return a real array.
+	const out: Array<Gamepad | undefined> = [];
 	const gamepads = navigator.getGamepads();
 	for (let i = 0; i < gamepads.length; i++) {
 		if (gamepads[i]) {
-			const id = gamepads[i].id;
-			ids[i] = aliases[id] ? aliases[id] : id;
+			out[i] = gamepads[i];
 		}
 	}
+	return out;
+}
+
+/**
+ * Returns an array of gamepad IDs (possibly aliased to friendlier names) for
+ * all connected gamepads.
+ */
+export function getGamepadIds(): Array<string | undefined> {
+	const ids: string[] = [];
+	getGamepads().forEach((gamepad, i) => {
+		if (gamepad) {
+			const id = gamepad.id;
+			ids[i] = aliases[id] ? aliases[id] : id;
+		}
+	});
 	return ids;
 }

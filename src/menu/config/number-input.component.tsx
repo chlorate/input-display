0fa0bf7c6daadcbb6f4ca1bfc4f8c1330f91e3e0
Chlorate <me@chlorate.ca>
2017-10-14 01:18:00 -0300

    Add NumberInputComponent

---
+++ src/menu/config/number-input.component.tsx
@@ -0,0 +1,44 @@
+interface Props {
+	className?: string;
+	id: string;
+	label: string;
+	suffix: string;
+	value: number;
+	min: number;
+	max: number;
+	help?: string;
+	onChange;
+}
+
+/**
+ * A generic number field.
+ */
+export const NumberInputComponent = (props: Props) => (
+	<div className={`form-group ${props.className || ""}`}>
+		<label for={props.id}>
+			{props.label}
+		</label>
+		<div className="input-group input-group-number">
+			<input
+				type="number"
+				className="form-control"
+				id={props.id}
+				value={props.value}
+				min={props.min}
+				max={props.max}
+				placeholder={props.min}
+				required
+				aria-describedby={`${props.id}-addon ${props.help ? `${props.id}-help` : ""}`}
+				onChange={props.onChange}
+			/>
+			<span className="input-group-addon" id={`${props.id}-addon`}>
+				{props.suffix}
+			</span>
+		</div>
+		{props.help !== undefined &&
+			<small className="form-text text-muted" id={`${props.id}-help`}>
+				{props.help}
+			</small>
+		}
+	</div>
+);
--- src/menu/config/poll-rate-input.component.tsx
+++ src/menu/config/poll-rate-input.component.tsx
@@ -2,6 +2,7 @@ import {linkEvent} from "inferno";
 import {connect} from "inferno-mobx";
 import {Config, maxPollRate, minPollRate} from "../../config/config";
 import {Store} from "../../storage/store";
+import {NumberInputComponent} from "./number-input.component";
 
 interface Props {
 	config: Config;
@@ -11,31 +12,16 @@ interface Props {
  * A numeric field for setting the controller poll rate.
  */
 export const PollRateInputComponent = connect([Store.Config], ({config}: Props) => (
-	<div className="form-group mb-0">
-		<label for="config-poll-rate">
-			Poll rate
-		</label>
-		<div className="input-group input-group-poll-rate">
-			<input
-				type="number"
-				className="form-control"
-				id="config-poll-rate"
-				value={config.pollRate}
-				min={minPollRate}
-				max={maxPollRate}
-				placeholder={minPollRate}
-				required
-				aria-describedby="config-poll-rate-addon config-poll-rate-help"
-				onChange={linkEvent(config, handleChange)}
-			/>
-			<span className="input-group-addon" id="config-poll-rate-addon">
-				Hz
-			</span>
-		</div>
-		<small className="form-text text-muted" id="config-poll-rate-help">
-			How often the controller's inputs are read.
-		</small>
-	</div>
+	<NumberInputComponent
+		id="config-poll-rate"
+		label="Poll rate"
+		suffix="Hz"
+		value={config.pollRate}
+		min={minPollRate}
+		max={maxPollRate}
+		help="How often the controller's inputs are read."
+		onChange={linkEvent(config, handleChange)}
+	/>
 ));
 
 function handleChange(config: Config, event): void {

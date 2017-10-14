a5604789be1cc9fd9b18a5af080d3fee9c76cda1
Chlorate <me@chlorate.ca>
2017-10-14 01:31:57 -0300

    Add CheckboxInputComponent

---
+++ src/menu/config/checkbox-input.component.tsx
@@ -0,0 +1,30 @@
+interface Props {
+	id?: string;
+	label: string;
+	checked: boolean;
+	help?: string;
+	onClick;
+}
+
+/**
+ * A generic checkbox field.
+ */
+export const CheckboxInputComponent = (props: Props) => (
+	<div class="form-check">
+		<label class="form-check-label">
+			<input
+				type="checkbox"
+				className="form-check-input"
+				checked={props.checked}
+				aria-describedby={props.help ? `${props.id}-help` : undefined}
+				onClick={props.onClick}
+			/>
+			{props.label}
+		</label>
+		{props.help !== undefined &&
+			<small className="form-text text-muted" id={`${props.id}-help`}>
+				{props.help}
+			</small>
+		}
+	</div>
+);

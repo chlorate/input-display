f6a7b76553526fed140c5a7c1a868ebaf5d80f1b
Chlorate <me@chlorate.ca>
2017-10-15 17:36:39 -0300

    Add ButtonReferenceSelectComponent

---
+++ src/menu/config/button-reference-select.component.tsx
@@ -0,0 +1,81 @@
+import {linkEvent} from "inferno";
+import {connect} from "inferno-mobx";
+import {ButtonReference} from "../../config/button-reference";
+import {DpadButtonReference} from "../../config/dpad-button-reference";
+import {NormalButtonReference} from "../../config/normal-button-reference";
+import {Controller} from "../../controller/controller";
+import {DpadButton} from "../../controller/dpad-button";
+import {NormalButton} from "../../controller/normal-button";
+import {clampIndex} from "../../math/util";
+import {Store} from "../../storage/store";
+
+interface Props {
+	controller: Controller;
+	className?: string;
+	id: string;
+	optional?: boolean;
+	reference?: ButtonReference;
+	onChange: LinkedEvent;
+}
+
+interface LinkedEvent {
+	data: any;
+	event: (data: any, reference?: ButtonReference) => void;
+}
+
+const notFound = -1;
+
+/**
+ * A field for selecting a button.
+ */
+export const ButtonReferenceSelectComponent = connect([Store.Controller], (props: Props) => {
+	let index: number | undefined;
+	if (props.reference !== undefined) {
+		const button = props.reference.resolve(props.controller);
+		if (button !== undefined) {
+			index = props.controller.buttons.indexOf(button);
+		} else {
+			index = notFound;
+		}
+	}
+
+	return (
+		<div class={`"form-group form-group-button-reference ${props.className || ""}`}>
+			<label for={props.id}>
+				Button
+			</label>
+			<select
+				className="form-control"
+				id={props.id}
+				value={index !== undefined ? index : ""}
+				required
+				onChange={linkEvent(props, handleChange)}
+			>
+				{props.optional || index === undefined &&
+					<option value="">None</option>
+				}
+				{props.controller.buttons.map((button, i) => (
+					<option value={i}>{button.name}</option>
+				))}
+				{props.reference !== undefined && index === notFound &&
+					<option value={index}>{props.reference.name}</option>
+				}
+			</select>
+		</div>
+	);
+});
+
+function handleChange(props: Props, event) {
+	let reference: ButtonReference | undefined;
+	if (event.target.value) {
+		const i = clampIndex(event.target.value);
+		const button = props.controller.buttons[i];
+		if (button instanceof NormalButton) {
+			reference = new NormalButtonReference(button.index);
+		} else if (button instanceof DpadButton) {
+			reference = new DpadButtonReference(button.direction);
+		}
+	}
+
+	props.onChange.event(props.onChange.data, reference);
+}

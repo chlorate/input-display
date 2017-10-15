f5967d37452debeb4fc1d61648a7d7bfb795d14f
Chlorate <me@chlorate.ca>
2017-10-15 14:03:21 -0300

    Add TextInputComponent

---
+++ src/menu/config/text-input.component.tsx
@@ -0,0 +1,27 @@
+interface Props {
+	className?: string;
+	id: string;
+	label: string;
+	value: string;
+	maxLength: number;
+	onChange;
+}
+
+/**
+ * A generic text field.
+ */
+export const TextInputComponent = (props: Props) => (
+	<div className={`form-group ${props.className || ""}`}>
+		<label for={props.id}>
+			{props.label}
+		</label>
+		<input
+			type="text"
+			className="form-control"
+			id={props.id}
+			value={props.value}
+			maxLength={props.maxLength}
+			onChange={props.onChange}
+		/>
+	</div>
+);

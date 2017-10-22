7568ab52234b4f75882c2687eb1a3e83b2fc0cd4
Chlorate <me@chlorate.ca>
2017-10-22 20:13:56 -0300

    Simplify EllipseControlComponent

--- src/display/ellipse-control.component.tsx
+++ src/display/ellipse-control.component.tsx
@@ -1,30 +1,25 @@
 import {connect} from "inferno-mobx";
 import {EllipseControl} from "../control/ellipse-control";
-import {Controller} from "../controller/controller";
-import {Store} from "../storage/store";
 import {ControlGroupComponent} from "./control-group.component";
 import {ControlLabelComponent} from "./control-label.component";
 
 interface Props {
-	controller: Controller;
 	control: EllipseControl;
 }
 
 /**
  * Draws a EllipseControl.
  */
-export const EllipseControlComponent = connect([Store.Controller], ((props: Props) => {
-	return (
-		<ControlGroupComponent control={props.control}>
-			<ellipse
-				className="control-button"
-				cx={props.control.centerX}
-				cy={props.control.centerY}
-				rx={props.control.centerX}
-				ry={props.control.centerY}
-				stroke-width={props.control.borderWidth}
-			/>
-			<ControlLabelComponent control={props.control} />
-		</ControlGroupComponent>
-	);
-}));
+export const EllipseControlComponent = connect(({control}: Props) => (
+	<ControlGroupComponent control={control}>
+		<ellipse
+			className="control-button"
+			cx={control.centerX}
+			cy={control.centerY}
+			rx={control.centerX}
+			ry={control.centerY}
+			stroke-width={control.borderWidth}
+		/>
+		<ControlLabelComponent control={control} />
+	</ControlGroupComponent>
+));

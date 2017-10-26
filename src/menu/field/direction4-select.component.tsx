fbe9a2115228b2f3aac71e1165f05175ef17f20f
Chlorate <me@chlorate.ca>
2017-10-26 20:40:47 -0300

    Add Direction4SelectComponent

---
+++ src/menu/field/direction4-select.component.tsx
@@ -0,0 +1,29 @@
+import {Direction4, direction4Names, sortedDirection4s} from "../../direction/direction4";
+
+interface Props {
+	className?: string;
+	id: string;
+	direction: Direction4;
+	onChange;
+}
+
+/**
+ * A field for selecting a cardinal direction.
+ */
+export const Direction4SelectComponent = (props: Props) => (
+	<div class={`form-group form-group-direction4 ${props.className || ""}`}>
+		<label for={props.id}>
+			Direction
+		</label>
+		<select
+			className="form-control"
+			id={props.id}
+			value={props.direction}
+			onChange={props.onChange}
+		>
+			{sortedDirection4s.map((direction) => (
+				<option value={direction}>{direction4Names[direction]}</option>
+			))}
+		</select>
+	</div>
+);

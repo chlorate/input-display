10edd2d1655950791bf2d9448b4c1162a3c46532
Chlorate <me@chlorate.ca>
2018-06-27 15:45:47 -0300

    LabelSelectComponent: migrate mobx, fix children

--- src/menu/field/label-select.component.tsx
+++ src/menu/field/label-select.component.tsx
@@ -1,4 +1,4 @@
-import {connect} from "inferno-mobx";
+import {observer} from "inferno-mobx";
 import {LabelPosition, labelPositionNames, sortedLabelPositions} from "../../control/label-position";
 import {LabelReplacement, labelReplacementNames, sortedLabelReplacements} from "../../control/label-replacement";
 
@@ -14,28 +14,36 @@ interface Props {
 /**
  * A dropdown for selecting a position of a label.
  */
-export const LabelSelectComponent = connect((props: Props) => (
-	<div className={
-		"form-group " +
-		`form-group-label-${props.replacement ? "replacement" : "position"} ` +
-		(props.className || "")
-	}>
-		<label for={props.id}>
-			{props.label}
-		</label>
-		<select
-			className={`form-control form-control-label-${props.replacement ? "replacement" : "position"}`}
-			id={props.id}
-			value={props.value || ""}
-			onChange={props.onChange}
-		>
-			<option value="">Hidden</option>
-			{props.replacement && sortedLabelReplacements.map((replacement) => (
-				<option value={replacement}>{labelReplacementNames[replacement]}</option>
-			))}
-			{sortedLabelPositions.map((position) => (
-				<option value={position}>{labelPositionNames[position]}</option>
-			))}
-		</select>
-	</div>
-));
+export const LabelSelectComponent = observer((props: Props) => {
+	var classSuffix = props.replacement ? "replacement" : "position";
+
+	var options = [<option value="">Hidden</option>];
+	if (props.replacement) {
+		sortedLabelReplacements.forEach((r) => {
+			options.push(<option value={r}>{labelReplacementNames[r]}</option>);
+		});
+	}
+	sortedLabelPositions.forEach((p) => {
+		options.push(<option value={p}>{labelPositionNames[p]}</option>);
+	});
+			
+	return (
+		<div className={
+			"form-group " +
+			`form-group-label-${classSuffix} ` +
+			(props.className || "")
+		}>
+			<label htmlFor={props.id}>
+				{props.label}
+			</label>
+			<select
+				className={`form-control form-control-label-${classSuffix}`}
+				id={props.id}
+				value={props.value || ""}
+				onChange={props.onChange}
+			>
+				{options}
+			</select>
+		</div>
+	);
+});

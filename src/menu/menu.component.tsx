5d65a904806373fd7241d0954fe0c8bb12823415
Chlorate <me@chlorate.ca>
2018-08-08 20:25:35 -0300

    MenuComponent: migrate mobx and router (partially)

--- src/menu/menu.component.tsx
+++ src/menu/menu.component.tsx
@@ -1,8 +1,7 @@
 import EventEmitter from "events";
-import createMemoryHistory from "history/createMemoryHistory";
-import Component from "inferno-component";
-import {connect} from "inferno-mobx";
-import {IndexRoute, Route, Router} from "inferno-router";
+import {Component} from "inferno";
+import {inject} from "inferno-mobx";
+import {MemoryRouter, Route, Switch} from "inferno-router";
 import {Config} from "../config/config";
 import {Event} from "../event";
 import {Store} from "../storage/store";
@@ -12,7 +11,7 @@ import {HelpComponent} from "./help/help.component";
 import {MenuButtonComponent} from "./menu-button.component";
 import {MenuCardComponent} from "./menu-card.component";
 
-interface Props {
+interface InjectedProps {
 	config: Config;
 	events: EventEmitter;
 }
@@ -21,32 +20,39 @@ interface Props {
  * Renders the menu. A router is used to navigate between tabs and closing the
  * menu.
  */
-@connect([Store.Config, Store.Events])
-export class MenuComponent extends Component<Props, {}> {
-	private history;
-
-	constructor(props: Props) {
-		super(props);
-		this.history = createMemoryHistory({
-			// Open menu on startup if no controls are defined.
-			initialEntries: [props.config.controls.length ? "/" : "/config"],
-		});
+@inject(Store.Config, Store.Events)
+export class MenuComponent extends Component {
+	private get injected(): InjectedProps {
+		return this.props as InjectedProps;
 	}
 
-	public render() {
+	public render(): JSX.Element {
+		// Open menu on startup if no controls are defined.
+		let entry = "/";
+		if (!this.injected.config.controls.length) {
+			entry = "/config";
+		}
+
+		// TODO: onUpdate={() => this.handleUpdate()}
+		// No more onUpdate
 		return (
-			<Router history={this.history} onUpdate={() => this.handleUpdate()}>
-				<IndexRoute component={MenuButtonComponent} />
-				<Route component={MenuCardComponent}>
-					<Route path="/config" component={ConfigComponent} />
-					<Route path="/controller" component={ControllerComponent} />
-					<Route path="/help" component={HelpComponent} />
-				</Route>
-			</Router>
+			<MemoryRouter initialEntries={[entry]}>
+				<Switch>
+					<Route exact path="/" component={MenuButtonComponent} />
+					<Route component={MenuCardComponent}>
+						<Route path="/config" component={ConfigComponent} />
+						<Route
+							path="/controller"
+							component={ControllerComponent}
+						/>
+						<Route path="/help" component={HelpComponent} />
+					</Route>
+				</Switch>
+			</MemoryRouter>
 		);
 	}
 
 	private handleUpdate(): void {
-		this.props.events.emit(Event.NavigateTab);
+		this.injected.events.emit(Event.NavigateTab);
 	}
 }

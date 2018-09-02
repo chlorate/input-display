b8474dcbe24e0c9889c723bc0d6847efa7c565f9
Chlorate <me@chlorate.ca>
2018-09-02 14:27:34 -0300

    Add new NumberInput component

--- src/menu/config/field/index.ts
+++ src/menu/config/field/index.ts
@@ -1,3 +1,4 @@
 export * from "./axis-index-select";
 export * from "./axis-reference-select";
 export * from "./axis-select";
+export * from "./number-input";
---
+++ src/menu/config/field/number-input.tsx
@@ -0,0 +1,69 @@
+import {ChangeEvent, Component, VNode} from "inferno";
+import {Input} from "inferno-bootstrap";
+import {observer} from "inferno-mobx";
+import {action, observable} from "mobx";
+import {clamp} from "../../../math/util";
+
+interface Props {
+	id?: string;
+	value: number;
+	min: number;
+	max: number;
+	defaultValue: number;
+	describedBy?: string;
+	onChange: (value: number) => void;
+}
+
+/**
+ * A number input field with extra error handling for invalid values.
+ */
+@observer
+export class NumberInput extends Component<Props> {
+	@observable
+	private dirtyValue?: string;
+
+	public render(): VNode {
+		const {id, min, max, defaultValue, describedBy} = this.props;
+		return (
+			<Input
+				id={id}
+				type="number"
+				value={this.value}
+				min={`${min}`}
+				max={`${max}`}
+				placeholder={`${defaultValue}`}
+				required
+				aria-describedby={describedBy}
+				onBlur={this.handleBlur}
+				onInput={this.handleInput}
+			/>
+		);
+	}
+
+	private get value(): string {
+		if (this.dirtyValue !== undefined) {
+			return this.dirtyValue;
+		}
+		return `${this.props.value}`;
+	}
+
+	@action
+	private handleBlur = (): void => {
+		this.dirtyValue = undefined;
+	};
+
+	@action
+	private handleInput = (event: ChangeEvent<HTMLInputElement>): void => {
+		const {min, max, defaultValue, onChange} = this.props;
+		const {value: dirtyValue} = event.target;
+
+		const value = parseFloat(dirtyValue);
+		if (isNaN(value)) {
+			onChange(defaultValue);
+		} else {
+			onChange(clamp(value, min, max));
+		}
+
+		this.dirtyValue = dirtyValue;
+	};
+}

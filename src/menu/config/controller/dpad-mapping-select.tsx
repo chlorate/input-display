8a563bb8729de81c717043bd36877ba93f7c3238
Chlorate <me@chlorate.ca>
2018-09-02 11:45:28 -0300

    Refactor DpadMappingSelect

--- src/menu/config/controller/dpad-mapping-select.tsx
+++ src/menu/config/controller/dpad-mapping-select.tsx
@@ -1,12 +1,12 @@
-import {linkEvent} from "inferno";
-import {connect} from "inferno-mobx";
+import {ChangeEvent, Component, VNode} from "inferno";
+import {FormGroup, FormText, Input, Label} from "inferno-bootstrap";
+import {inject, observer} from "inferno-mobx";
+import {Link} from "inferno-router";
 import {action} from "mobx";
-import {AxisReference} from "../../config/axis-reference";
-import {Config} from "../../config/config";
-import {Controller} from "../../controller/controller";
-import {Store} from "../../storage/store";
-import {AxisIndexSelectComponent} from "../field/axis-index-select.component";
-import {AxisReferenceSelectComponent} from "../field/axis-reference-select.component";
+import {AxisReference} from "../../../config/axis-reference";
+import {Config} from "../../../config/config";
+import {Controller} from "../../../controller/controller";
+import {Store} from "../../../storage/store";
 
 const defaultXAxisIndex = 0;
 const defaultYAxisIndex = 1;
@@ -17,96 +17,77 @@ enum Mapping {
 	DualAxes = "dualAxes",
 }
 
-interface Props {
+interface InjectedProps {
 	config: Config;
 	controller: Controller;
 }
 
 /**
- * A set of fields for selecting which d-pad mapping to use. When one of the
- * axis mappings are selected, fields are shown that allow the user to select
- * which axes to use.
+ * A field for selecting which d-pad mapping to use.
  */
-export const DpadMappingFieldsetComponent = connect([Store.Config, Store.Controller], (props: Props) => {
-	let value = Mapping.Buttons;
-	if (props.config.dpadAxisIndex !== undefined) {
-		value = Mapping.SingleAxis;
-	}
-	if (props.config.dpadXAxis !== undefined && props.config.dpadYAxis !== undefined) {
-		value = Mapping.DualAxes;
+@inject(Store.Config, Store.Controller)
+@observer
+export class DpadMappingSelect extends Component {
+	private get injected(): InjectedProps {
+		return this.props as InjectedProps;
 	}
 
-	return (
-		<fieldset>
-			<div className="form-group">
-				<label htmlFor="config-dpad-mapping">
-					D-pad mapping
-				</label>
-				<select
-					className="form-control"
-					id="config-dpad-mapping"
-					value={value}
-					onChange={linkEvent(props, handleChangeMapping)}
-				>
-					<option value={Mapping.Buttons}>Buttons</option>
-					<option value={Mapping.SingleAxis}>Single axis</option>
-					<option value={Mapping.DualAxes}>Dual axes</option>
-				</select>
-				<small className="form-text text-muted">
-					Controllers vary in how d-pad inputs are mapped. Directions
-					could be mapped to buttons, to values on a single axis, or to
-					separate X and Y axes. Go to the Controller tab and move the
-					d-pad to determine which mapping fits your controller.
-				</small>
-			</div>
-			{value === Mapping.SingleAxis &&
-				<AxisIndexSelectComponent
-					id="config-dpad-axis-index"
-					label="D-pad axis"
-					value={props.config.dpadAxisIndex}
-					onChange={linkEvent(props.config, handleChangeIndex)}
-				/>
-			}
-			{value === Mapping.DualAxes &&
-				<div className="form-row">
-					<AxisReferenceSelectComponent
-						className="col"
-						id="config-dpad-x-axis"
-						label="D-pad X axis"
-						reference={props.config.dpadXAxis}
-						required={true}
-					/>
-					<AxisReferenceSelectComponent
-						className="col"
-						id="config-dpad-y-axis"
-						label="D-pad Y axis"
-						reference={props.config.dpadYAxis}
-						required={true}
-					/>
-				</div>
-			}
-		</fieldset>
+	public render = (): VNode => (
+		<FormGroup>
+			<Label for="config-dpad-mapping">Mapping</Label>
+			<Input
+				className="w-auto"
+				id="config-dpad-mapping"
+				type="select"
+				value={this.value}
+				aria-describedby="config-dpad-mapping-help"
+				onChange={this.handleChange}
+			>
+				<option value={Mapping.Buttons}>Buttons</option>
+				<option value={Mapping.SingleAxis}>Single axis</option>
+				<option value={Mapping.DualAxes}>Dual axes</option>
+			</Input>
+			<FormText id="config-dpad-mapping-help">
+				Controllers vary in how d-pad inputs are mapped. Directions
+				could be mapped to buttons, to values on a single axis, or to
+				separate X and Y axes. To determine which mapping fits your
+				controller, go to the{" "}
+				<Link to="/controller">Controller tab</Link>, move the d-pad
+				around, and watch what changes. If buttons are pressed, use the
+				buttons mapping. If only one axis changes, use the single axis
+				mapping. If two axes change, use the dual axes mapping.
+			</FormText>
+		</FormGroup>
 	);
-});
 
-const handleChangeMapping = action((props: Props, event): void => {
-	switch (event.target.value) {
-		case Mapping.Buttons:
-			props.config.clearDpadMapping();
-			break;
-		case Mapping.SingleAxis:
-			// Usually the last axis is the one to use.
-			props.config.dpadAxisIndex = props.controller.axes.length - 1;
-			break;
-		case Mapping.DualAxes:
-			props.config.setDpadDualAxes(
-				new AxisReference(defaultXAxisIndex, false),
-				new AxisReference(defaultYAxisIndex, false),
-			);
-			break;
+	private get value(): Mapping {
+		const {dpadAxisIndex, dpadXAxis, dpadYAxis} = this.injected.config;
+		if (dpadAxisIndex !== undefined) {
+			return Mapping.SingleAxis;
+		}
+		if (dpadXAxis !== undefined && dpadYAxis !== undefined) {
+			return Mapping.DualAxes;
+		}
+		return Mapping.Buttons;
 	}
-});
 
-const handleChangeIndex = action((config: Config, event): void => {
-	config.dpadAxisIndex = event.target.value;
-});
+	@action
+	private handleChange = (event: ChangeEvent<HTMLSelectElement>): void => {
+		const {config, controller} = this.injected;
+		switch (event.target.value) {
+			case Mapping.Buttons:
+				config.clearDpadMapping();
+				break;
+			case Mapping.SingleAxis:
+				// Usually the last axis is the one to use.
+				config.dpadAxisIndex = controller.axes.length - 1;
+				break;
+			case Mapping.DualAxes:
+				config.setDpadDualAxes(
+					new AxisReference(defaultXAxisIndex, false),
+					new AxisReference(defaultYAxisIndex, false),
+				);
+				break;
+		}
+	};
+}
